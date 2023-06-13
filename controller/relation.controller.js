bb1f9dffc829df9dc3844fdfaa162bd2ad839576
yaser ahmadi <m.y.ahmadi22@gmail.com>
2023-06-13 17:59:13 +0330

    [feature]: added label to edges

--- controller/relation.controller.js
+++ controller/relation.controller.js
@@ -44,15 +44,16 @@ async function createRelation(req, res, next) {
     let session;
     try {
         session = neo4j().session();
-        const { from, to } = req.body;
+        const { from, to, meta, label } = req.body;
+        const { description, level } = meta;
         const result = await session.run(
             `
             MATCH (a)
             MATCH (b)
             WHERE elementId(a) = $from AND elementId(b)=$to
-            CREATE (a)-[rel:FOLLOW]->(b)
+            CREATE (a)-[rel:FOLLOW {label: $label, description: $description, level:$level}]->(b)
             RETURN rel
-            `,{ from: from, to: to }
+            `, { from: from, to: to, label, description, level }
         )
         return res.send({
             result: result.records
